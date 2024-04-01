fc009a6e7d06b283abb5980ba267a1867adb7546
Blackbox19 <trevorirwin819@gmail.com>
2024-03-31 22:54:43 -0400

    submit, drag/drop and color still not functional

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Task-Board
+
+## Description
+
+This webpage is a task board I made for the Rutgers coding bootcamp. The assignment entailed creating and deploying a task board using third party API's to meet a list of criteria. I was taksed with creating a task list that uses a Bootstrap modal form to collect input from a user and store it to local storage. Once the data is submitted the modal will be hidden and the task that the user enters will be displayed in tthe "To Do" section. They can make multiple entries that will display one after another and up to about 5 megabytes worth before there is no storage left. Tasks will be draggable and droppable thanks to Jquery. This project really tested my knowledge of Javascript logic and Jquery functions/methods. One of the hardest concepts was how to implament the draggable and droppable functions and im still not 100% sure how it works, but I'll get there.  
+
+## Visuals
+
+<img src="./assets/images/" >
+
+<img src="./assets/images/" >
+
+<img src="./assets/images/" >
+
+<img src="./assets/images/" >
+
+<img src="./assets/images/" >
+
+## Deployment
+
+Provided as a link here - 
+
+## Usage
+
+To use the webpage, you click the "Add Task" button and a pop up form will appear. you will then input the task, due date, and short description of what you want to log on the task board. Multiple entries cxan be made and will be styled in color based on weather it is upcoming, due soon, or past due. This was acheved with Dayjs API Try it out! To inspect each element, you can open the Chrome DevTools by pressing Command+Option+I (macOS) or Control+Shift+I (Windows). A console panel should open either below or to the side of the webpage in the browser. There you can navigate to the Elements tab to check out the HTML, CSS, or Javascript. Also you can click on the Application tab and click the local storage dropdown. Procede to click on the folder that appears and there you will see your tasks stored in the local storage of your browser on your device.
+## Credits
+
+JD Tadlock - My Instructor for the Rutgers University Coding Bootcamp for all the help.
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,141 +1,144 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks"))
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 0
 
 // Todo: create a function to generate a unique task id
-// use math rendom to generate a task id
 function generateTaskId() {
-    const randomId = Math.floor(Math.random() * 1000) + 100;
-    // console.log(randomId);
-    return randomId
+    nextId++
+    return nextId
 }
-
-
-
-
 
 // Todo: create a function to create a task card
-const todoOutput = $('#todo-cards')
-const tasks = JSON.parse(localStorage.getItem('formArray')) || []
-
 function createTaskCard(task) {
+    const newTaskCard = (`
+    <div data-task='${task.id}' data-status='${task.status}' class="new-task my-4 p-3 rounded-5 border border-dark-subtle shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+        <h2 class="task-title">${task.taskTitle}</h2>
+        <p class="task-date">${task.description}</p>
+        <p id="date-color" class="task-desc">Due: ${task.dueDate}</p>
+        <button id="delete-task-btn" class="rounded btn btn btn-outline-light bg-danger bg-gradient">Delete</button>
+    </div>
+    `)
 
-    if (tasks.length) {
-        todoOutput.innerHTML = ''
-    }
-
-    for( let task of tasks) {
-        todoOutput.append(`
-        <div id="${generateTaskId()}" class="new-task my-4 p-3 rounded-3 border border-dark-subtle">
-            <h2 class="task-title">${task.taskTitle}</h2>
-            <p class="task-date">${task.description}</p>
-            <p class="task-desc">Due: ${task.dueDate}</p>
-            <button id="delete-task-btn" class="rounded btn btn-outline-danger">Delete</button>
-        </div>
-        `)
-    }
+    return newTaskCard
 }
-
-createTaskCard()
 
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    
+    let taskList = JSON.parse(localStorage.getItem("formArray")) || []
+    
+    taskList.forEach(task => {
+        const newTaskCard = createTaskCard(task)
+        const todoLane = $('#todo-cards')
+        // const targetLane = $('.lanes[data-status="' + task.status + '"]')
+        todoLane.append(newTaskCard)
+        // targetLane.append(newTaskCard)
+        const newTask = $('.new-task')
+        if(dayjs().isAfter(task.dueDate, 'day')) {
+            newTask.addClass('bg-danger')
+        } else if (dayjs().isSame(task.dueDate, 'day')) {
+            newTask.addClass('bg-warning text-dark')
+        } else {
+            newTask.addClass('bg-light text-dark')
+        }
+    
+    })
 
+
+    const taskDrag = $('.new-task')
+    taskDrag.draggable({
+        revert: 'invalid',
+        containment: 'document',
+        opacity: 0.5,
+        zIndex: 100
+    })
+    
+    const deleteTask = $('#delete-task-btn')
+    deleteTask.on('click', handleDeleteTask)
 }
-
 // Todo: create a function to handle adding a new task
-const formData = $('#taskForm')
-const taskName = $('#nameInput')
-const dueDate = $('#dateInput')
-const description = $('#descriptionInput')
-
 function handleAddTask(e) {
     e.preventDefault()
-    const taskNameValue = taskName.val()
-    const dueDateValue = dueDate.val()
-    const descriptionValue = description.val()
-    
+    const taskNameValue = $('#nameInput').val()
+    const dueDateValue = $('#dateInput').val()
+    const descriptionValue = $('#descriptionInput').val()
+        
     const raw = localStorage.getItem('formArray')
     const taskArray = JSON.parse(raw) || []
+
+    const uniqueId = generateTaskId()
     
     const newTaskObj = {
+        id: uniqueId,
         taskTitle: taskNameValue,
-        dueDate: dueDateValue,
+        dueDate: dayjs(dueDateValue).format('MMM D, YYYY'),
         description: descriptionValue
     }
     
-    console.log(newTaskObj)
-
     taskArray.push(newTaskObj)
     
     localStorage.setItem('formArray', JSON.stringify(taskArray))
-    
-    // formData.trigger('reset')
-    // $('#formModal').css.style('display: none;')
-
+    localStorage.setItem('nextId', JSON.stringify(uniqueId))
+    window.location.reload()
+    // $('#formModal').modal('hide')
 }
-
 
 // Todo: create a function to handle deleting a task
-
 function handleDeleteTask(event) {
+    let taskId = $(event.target).closest('.new-task').data('task')
+    let taskArray = JSON.parse(localStorage.getItem("formArray")) || []
+    taskArray = taskArray.filter(task => task.id !== taskId)
+    localStorage.setItem('formArray', JSON.stringify(taskArray))
+    $(event.target).closest('.new-task').remove()
+    window.location.reload()
 }
 
+
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+function handleDrop() {
+    $('.lanes').droppable({
+        accept: '.new-task',
+
+    })
 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-const submitBtn =  $('#submit')
 $(document).ready(function () {
+    renderTaskList()
+    handleDrop()
+
+    const submitBtn =  $('#submit')
+    submitBtn.on('click', function(e) {
+        handleAddTask(e)
+        generateTaskId()
+        // createTaskCard()
+    })
 });
-
-submitBtn.on('click', function(event) {
-    handleAddTask(event)
-    generateTaskId()
-    createTaskCard()
-    window.location.reload()
-})
-// const submitBtn =  $('#submit')
-// function startSubmit() {
-//     submitBtn.on('click', handleAddTask)
-//     submitBtn.on('click', generateTaskId)
-//     submitBtn.on('click', createTaskCard)
-//     submitBtn.on($('#formModal').hide())
-// }
-
-// startSubmit()
-
  
-
-
-
-
-
 // ===========================================================
 // Light and dark mode
 
 // Gets the body element and assigns it to a variable
-// const body = document.body
+const body = document.body
 
 // Grabs the id tag assigned to the light/dark button
-// const darkToggle = document.getElementById('toggle')
+const darkToggle = document.getElementById('toggle')
 
-// const theme = localStorage.getItem('theme')
-// // if(theme){
-//     document.body.classList.add('dark-mode')
-// }
+const theme = localStorage.getItem('theme')
+if(theme){
+    document.body.classList.add('dark-mode')
+}
 
 // Adds an event listener to the button and when the button is pressed a class tag is applied to the body element which enables the css styles assigned to the .dark-mode class tag
-// darkToggle.addEventListener('click', function() {
-    // body.classList.toggle('dark-mode')
+darkToggle.addEventListener('click', function() {
+    body.classList.toggle('dark-mode')
     
-    // if(document.body.classList.contains('dark-mode')) {
-        // localStorage.setItem('theme', 'dark-mode')
+    if(document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode')
         
-    // } else{
-        // localStorage.removeItem('theme')
-    // }
-// })
+    } else{
+        localStorage.removeItem('theme')
+    }
+})
